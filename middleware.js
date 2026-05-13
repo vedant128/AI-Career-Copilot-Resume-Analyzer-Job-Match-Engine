0c3faae72e95f49c8bad68ff3eb10338cd74a074
vedant128 <vedantgunjal2005@email.com>
2026-05-13 23:52:16 +0530

    Extreme middleware simplification

--- middleware.js
+++ middleware.js
@@ -1,19 +1,6 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware(async (auth, req) => {
-  const protectedPaths = ["/appointments", "/explore", "/dashboard", "/onboarding"];
-  const isProtected = protectedPaths.some((path) =>
-    req.nextUrl.pathname.startsWith(path)
-  );
-
-  if (isProtected) {
-    const { userId } = await auth();
-    if (!userId) {
-      const { redirectToSignIn } = await auth();
-      return redirectToSignIn();
-    }
-  }
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
